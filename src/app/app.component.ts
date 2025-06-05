eafe9305bbb8199d5ed0a58c94b4420429046224
Ramukannu Suguna <sugunaramukkannu@gmail.com>
2025-06-05 15:00:16 +0800

    updated title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bitcoin';
+  title = 'bitcoins';
 
   constructor(private router: Router) { }
 
